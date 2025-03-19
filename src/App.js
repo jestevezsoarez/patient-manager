5ad45d63c3b3424b70e16d815b97f4abf5c5f011
Javier Estevez <javierdaniloestevez@gmail.com>
2025-03-19 19:02:40 -0300

    create a state for the appoinment and appoinments, and store inputs values in form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+
+  const [appointments, setAppointments] = useState([]);
+
+  const [appointment, setAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    hour: '',
+    symptoms: ''
+  });
+
+  const updateAppointment = (e) => {
+    setAppointment({
+        ...appointment,
+        [e.target.name]: e.target.value
+      })
+  }
+
   return (
     <>
       <h1>Patient Manager</h1>
@@ -8,7 +27,10 @@ function App() {
         <div className="row">
           <div className="one-half column">
             <h2>Create Appointment</h2>
-            <Form />
+            <Form
+              appointment={appointment}
+              updateAppointment={updateAppointment}
+            />
           </div>
           <div className="one-half column">
             <h2>Scheduler</h2>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({appointment, updateAppointment}) => {
+
+    const { pet, owner, date, hour, symptoms } = appointment;
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log('submit de formulario');
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={submitForm}>
             <label>Pet Name</label>
             <input 
                 type='text'
                 className='u-full-width'
                 placeholder='Pet Name'
                 name='pet'
+                value={pet}
+                onChange={updateAppointment}
             />
             <label>Owner Name</label>
             <input 
@@ -16,23 +27,31 @@ const Form = () => {
                 className='u-full-width'
                 placeholder='Owner Name'
                 name='owner'
+                value={owner}
+                onChange={updateAppointment}
             />
             <label>Date</label>
             <input 
                 type='date'
                 className='u-full-width'
                 name='date'
+                value={date}
+                onChange={updateAppointment}
             />
             <label>Hour</label>
             <input 
                 type='time'
                 className='u-full-width'
                 name='hour'
+                value={hour}
+                onChange={updateAppointment}
             />
             <label>Symptoms</label>
             <textarea
                 className='u-full-width'
                 name='symptoms'
+                value={symptoms}
+                onChange={updateAppointment}
             ></textarea>
             <button
                 className='button-primary u-full-width'
